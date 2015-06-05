5d4d47950e60a695512b034bf3f06e69c8de47e4
Commander-lol <ljcapitanio@gmail.com>
2015-06-05 15:00:04 +0100

    Added tests for basic tags

---
+++ test/SimpleTagTest.js
@@ -0,0 +1,35 @@
+var sb = require("../sideburns"),
+    fs = require("fs"),
+    assert = require("assert"),
+    template,
+    data = {
+        firstname: "john",
+        lastname: "doe",
+        age: 37,
+        hasCat: true,
+        foo: {
+            bar: {
+                fizz: "Buzz"
+            }
+        }
+    },
+    options = {},
+    output;
+
+template = fs.readFileSync("./files/simpletags.sb").toString();
+output = sb(template, data, options);
+assert.strictEqual(output, "My name is john doe, I am 37 years old. It is true that I have a cat.");
+
+template = fs.readFileSync("./files/simpletagserror.sb").toString();
+assert.throws(function(){
+    output = sb(template, data, options);
+}, /missing expected data "missing"/);
+
+template = fs.readFileSync("./files/simpletagsdeep.sb").toString();
+output = sb(template, data, options);
+assert.strictEqual(output, "My name is john doe, I am 37 years old. It is true that I have a cat, but I bet you don't know that my favourite astronaut is Buzz Aldrin.");
+
+template = fs.readFileSync("./files/simpletagsdeeperror.sb").toString();
+assert.throws(function(){
+    output = sb(template, data, options);
+}, /missing expected data "fizz\.bar" \(IDENT: foo\.fizz\.bar\)/);
